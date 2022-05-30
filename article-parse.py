426d2ee7972232b606849258aed0b4b88e940a30
Salwyn Mathew <salwynmathew13@gmail.com>
2022-05-30 21:54:05 +0530

    added getTags

--- article-parse.py
+++ article-parse.py
@@ -1,10 +1,17 @@
 from newspaper import Article
+import nltk
 
-url = input("")
-article = Article(url)
-article.download()
-article.parse()
-article.nlp()
+def getTags(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    tags = list(article.keywords)
+    for i in range(len(tags)):
+        if tags[i][-1] in tags:
+            tags[i] = tags[i][:-1]
+    tags = list(set(tags))
+    return tags
 
-tags = list(article.keywords)
-print(tags[5])
+url = input("Enter article URL: ")
+getTags(url)
