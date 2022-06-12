64ba282011df1e577a0ee4ab81c9b86a8c8a84b7
Salwyn Mathew <salwynmathew13@gmail.com>
2022-06-12 16:33:46 +0530

    added TwitterParse

--- twitter-parse.py
+++ twitter-parse.py
@@ -1,40 +1,44 @@
 from re import T
 import tweepy
 import nltk
-from nltk import word_tokenize, pos_tag_sents
+from nltk import word_tokenize, pos_tag, pos_tag_sents
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
 
-consumer_key = 'jUXANq60QoEbLzt1YRiQeUVaO'
-consumer_secret = 'aPvmdRkI1sA0BEonChYHj5KoSoamlryPWdDiR4kqhNlVflA8Pu'
-access_token = '4662587646-MlfQI4uZBlV0hI8ge2wEupXCSWueyvwkd9uCiyr'
-access_token_secret = 'fAZFKy0snAeGn3zYAbsOcIxfTdI2YGnUQXah6cxdhr5e0'
+consumer_key = os.environ.get('CONSUM_KEY')
+consumer_secret = os.environ.get('CONSUM_SECR')
+access_token = os.environ.get('ACC_TOK')
+access_token_secret = os.environ.get('ACC_TOK_SEC')
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-# set access to user's access key and access secret 
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
-id= '1535569677971345409'
+def TwitterParse(id):
+    # set access to user's access key and access secret 
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    status = api.get_status(id, tweet_mode = 'extended')
 
-status = api.get_status(id, tweet_mode = 'extended')
-
-# if image exists in the tweet
-if 'media' in status.entities:
-    for image in  status.entities['media']:
-        print(image['media_url'])
-
-# fetching the text attribute
-text = status.full_text
-tokens = set(word_tokenize(text))
-tokens = nltk.pos_tag_sents(tokens)
-tagged_tok=[]
-for i in range(len(tokens)):
-  if tokens[i][1]=="NNP" or tokens[i][1]=="NNS" or tokens[i][1]=="NN":
-    tagged_tok.append(tokens[i][0])
-
-def removeStopWords(tagged_tok):
+    # check for meadia in the tweet
+    if 'media' in status.entities:
+        for image in  status.entities['media']:
+            print(image['media_url'])
+    
+    # fetching the text attribute
+    text = status.full_text
+    tokens = set(word_tokenize(text))
+    tokens = pos_tag(tokens)
+    # check for nouns in the token list
+    tagged_tok=[]
+    for i in range(len(tokens)):
+        if tokens[i][1]=="NNP" or tokens[i][1]=="NNS" or tokens[i][1]=="NN":
+            tagged_tok.append(tokens[i][0])
+    
+    # remove stop words
     stop_words = set(nltk.corpus.stopwords.words('english'))
     tokens = [word for word in tagged_tok if word not in stop_words]
     return tagged_tok
 
-print(removeStopWords(tagged_tok))
+id= '1535569677971345409'
+print(TwitterParse(id))
